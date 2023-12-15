8677f3f65a5412761313d5395db1b5d6cbd73290
Babajan <soacewoo@gmail.com>
2023-12-15 10:46:34 +0500

    main text continiue

--- src/components/Welcomer/Welcomer.jsx
+++ src/components/Welcomer/Welcomer.jsx
@@ -2,7 +2,10 @@ import Header from "../Header/Header";
 import classes from "./Welcomer.module.css";
 // import BgImage from "../assets/Landing background (Yellow).png";
 import HeaderText from "../../assets/logo/Text.svg";
-import Button from "../UI/Button";
+import Button from "../UI/Button/Button";
+
+import animalsImg from "../../assets/Animals images.png";
+import Card from "../UI/Card/Card";
 
 const Welcomer = () => {
   return (
@@ -10,22 +13,29 @@ const Welcomer = () => {
       <div>
         <Header />
       </div>
-      <div className={classes.grid}>
+      <div className={classes.flexContainer}>
         <div className={classes.mainText}>
           <img src={HeaderText} alt="" />
-          <p>
-            Онлайн-магазин домашних животных - удобное решение, когда лень
-            выходить из дома
-          </p>
+          <span className={classes.textOdd}>
+            <h4>
+              Онлайн-магазин домашних животных - удобное решение, когда лень
+              выходить из дома
+            </h4>
+          </span>
           <Button> button</Button>
         </div>
-        <div className={classes.petsCart}>
-          <div>cart with picture</div>
-          <div>two buttons with select pet</div>
-          <div>paginnation</div>
-          <div>Почему выбирают наших друзей,</div>
+        <div className={classes.picture}>
+          <img src={animalsImg} alt="" />
         </div>
-        <div className={classes.picture}>picture</div>
+      </div>
+      <div className={`${classes.buyPets} ${classes.flex}`}>
+        <div>
+          <Card className={classes.petsCard}>cart with picture</Card>
+          <Card className={classes.petsCard}>cart with picture</Card>
+        </div>
+        <div>two buttons with select pet</div>
+        <div>paginnation</div>
+        <div>Почему выбирают наших друзей,</div>
       </div>
     </div>
   );
